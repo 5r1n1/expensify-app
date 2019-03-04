f602727fb5b6499aec28ae397ad2ca56e1acb988
Srinivas Gopalakrishnan <srinig@msn.com>
2019-03-04 16:58:36 +0530

    Initial commit - expensify-app

---
+++ README.md
@@ -0,0 +1,4 @@
+# expensify-app
+Created as a part of online ReactJS Course
+
+Part 2 Section 9 : React Router
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom' 
+import './styles/styles.scss'
+import 'normalize-css/normalize.css'
+
+ReactDOM.render (<p>Expensify-app</p>, document.getElementById('app'));
