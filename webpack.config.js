fe4312e6418851c6cecb0cc5aa10e13ffe94a0c8
Srinivas Gopalakrishnan <srinig@msn.com>
2020-10-10 11:33:13 +0530

    Minified for Production
    Signed-off-by: Srinivas Gopalakrishnan <srinig@msn.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,49 @@
 const path = require ('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-    path: path.join (__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = env => {
+  const isProd = env === 'production';
+
+  return ({
+    entry: './src/app.js',
+    output: {
+      path: path.join (__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           }
-        }
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8080,
-    historyApiFallback: true,
-  }
+        },
+        {
+          test: /\.s?css$/i,
+          use: [MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            }, {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ]
+        },
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      compress: true,
+      port: 8080,
+      historyApiFallback: true,
+    }
+  });
 };
