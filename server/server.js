ebd92f924c91e47974afc9bc7df65a263930a9c7
Srinivas Gopalakrishnan <srinig@msn.com>
2020-10-11 10:30:05 +0530

    Changed Currency date formatting in expense list
    - Updated doube-slash typo in Index.html
    - Updated single quote typo in server.js
    
    Signed-off-by: Srinivas Gopalakrishnan <srinig@msn.com>

--- server/server.js
+++ server/server.js
@@ -10,4 +10,4 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {console.log(`'Server is running on port ${port}`);});
+app.listen(port, () => {console.log(`Server is running on port ${port}`);});
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = props =>
   <div>
     <Link to={'/edit/'+props.id}>
       <h3>Description: {props.description}</h3>
     </Link>
-    <div>Created@: {props.createdAt}</div>
-    <div>Amount: {props.amount}</div>
+    <div>Created@: {moment(props.createdAt).format('MMMM Do, YYYY')}</div>
+    <div>Amount:
+      {new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR' })
+        .format(props.amount / 100)}
+    </div>
     <div>Note: {props.note}</div>
     <p></p>
   </div>;
